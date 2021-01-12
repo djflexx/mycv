ddc049b1fba47acc0eba739c1ac5b8f3e5d37349
Idan Atias <idanatias2@gmail.com>
2021-01-12 21:38:36 +0200

    mobile fixing

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -16,7 +16,9 @@ export default function About() {
           I feel satisfied solving difficult and complicated problems.<br /> 
           Now , this hobby has become my biggest passion and my goal is<br /> to make it my way of living.
           </p>
-         <div className="about-image"></div> 
+         <div className="about-image">
+             <img src="https://previews.dropbox.com/p/thumb/ABA4UCwghjx7mAVeAc8WiiizzmVN5b6KK6uiaSFWm0Jgiwdocn8Qal5GfXXWW0YW0ycTWz1wMkpuTSlUYdhGqq18t6QL3qQsAt025r0icW9ywYXCoB88QN3JsozEhxY5AvY_phFSFWmu8SDM1MZp-KDvEwnHxnbTWw57AfQ3lI_5D_7ahdBvt2RtslkT2LimJt1bDoiYl2FU5Fh08zlkcgZvhIFp_TPO1a2ysogpKnZ-RRjVoBu_hQY_VYje8eWOOm4kplRX1dDsyAJCFNsPPhRS0jrjtmFTQQkHLFWRA-bbxL3Z4RWah_gS6OHGR_T0L614lZmTlr3Am6RxwOEhaDEkhMeB3_PY3Cc3l_MWc2Qhmg/p.jpeg?fv_content=true&size_mode=5" alt="me"></img>
+         </div> 
         </div>
     )
 }
