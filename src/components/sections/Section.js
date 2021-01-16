035c127bb16299020f10f54491cb025e3c0cc960
Idan Atias <idanatias2@gmail.com>
2021-01-16 16:05:50 +0200

    fixing mobile

--- src/components/sections/Section.js
+++ src/components/sections/Section.js
@@ -77,7 +77,7 @@ export default function Section() {
             {sections.map((section, index) => {
                 return (
                     <div className="container" key={index} onClick={handleClick}>
-                    <div className="section-heading">
+                    <div className="section-heading" onClick={handleClick}>
                       <Icon icon= {section.icon}/>
                       <SectionTitle content={section.content}/> 
                     </div>
