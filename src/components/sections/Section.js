ead4825535c84411a4fc7ed152a2431d70db11b2
Idan Atias <idanatias2@gmail.com>
2021-01-16 10:19:23 +0200

    mobile bug

--- src/components/sections/Section.js
+++ src/components/sections/Section.js
@@ -63,7 +63,7 @@ export default function Section() {
             {sections.map((section, index) => {
                 return (
                     <div className={!isClicked ? "container" : "container-open"} key={index} onClick={handleClick}>
-                    <div className={!isClicked ? "section-heading" : "heading-open"} onClick={handleClick}>
+                    <div className={!isClicked ? "section-heading" : "heading-open"}>
                       {!isClicked && <Icon icon= {section.icon}/> }
                       <SectionTitle content={section.content}/>
                     </div>
