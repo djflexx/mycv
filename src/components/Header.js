0f0adb990c21b9c73570cfd8d7b401f4658d4587
Idan Atias <idanatias2@gmail.com>
2021-01-26 15:34:08 +0200

    linkedin added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react'
 import './Header.css'
 
 export default function Header() {
-    const handleClick = () => {
-        window.location.href = "https://github.com/djflexx"
-        console.log(window.location)
+    const handleClick = (event) => {
+        if(event.target.id === "github"){
+            window.location.href = "https://github.com/djflexx"
+        }
+        if (event.target.id === "linkedin"){
+            window.location.href = "https://www.linkedin.com/in/idan-atiasdev/"
+        }
     }
     return (
         <div className="header">
@@ -12,8 +16,9 @@ export default function Header() {
               <h1>Idan Atias</h1>
               <p>Web-Developer</p>
            </div>
-           <div className="github-icon" onClick={handleClick}>
-              <img src="https://img.icons8.com/fluent/96/000000/github.png"/>
+           <div className="icons" onClick={handleClick}>
+              <img src="https://img.icons8.com/fluent/96/000000/github.png" id="github"/>
+              <img src="https://img.icons8.com/fluent/96/000000/linkedin.png" id="linkedin"/>
            </div> 
         </div>
     )
